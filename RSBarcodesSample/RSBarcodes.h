607c979b9c3ebc929c69f23935d88eee26a6820b
Daniele <Daniele@Psych-II.local>
2014-03-21 23:40:40 +0100

    Created RSBarcodes framework (iOS 5.1 and later, arm64 ready) added RSBarcodes.h

---
+++ RSBarcodesSample/RSBarcodes.h
@@ -0,0 +1,16 @@
+//
+//  RSBarCodes.h
+//  RSBarcodesSample
+//
+//  Created by Daniele on 21/03/14.
+//  Copyright (c) 2014 P.D.Q. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "RSCodeView.h"
+#import "RSCodeGen.h"
+
+@interface RSBarcodes : NSObject
+
+@end
---
+++ RSBarcodesSample/RSBarcodes.m
@@ -0,0 +1,13 @@
+//
+//  RSBarCodes.m
+//  RSBarcodesSample
+//
+//  Created by Daniele on 21/03/14.
+//  Copyright (c) 2014 P.D.Q. All rights reserved.
+//
+
+#import "RSBarcodes.h"
+
+@implementation RSBarcodes
+
+@end
